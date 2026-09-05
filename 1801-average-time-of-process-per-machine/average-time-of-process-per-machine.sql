e4806062658ebc881b10a5b449431ab343e35e6f
shikhar-BHU108 <121691864+108shikhar@users.noreply.github.com>
2026-09-06 02:24:16 +0530

    Time: 231 ms (96.46%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT t1.machine_id,
-ROUND(AVG(ROUND(t2.timestamp-t1.timestamp,3)),3) AS 'processing_time'
+ROUND(AVG(t2.timestamp-t1.timestamp),3) AS 'processing_time'
 FROM Activity t1
 INNER JOIN Activity t2
 ON t1.machine_id=t2.machine_id AND t1.process_id=t2.process_id
